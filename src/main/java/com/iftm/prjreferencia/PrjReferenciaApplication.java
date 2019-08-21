fd205fa6c0e74d5b2a6ea60ca440f228331a3d2c
Rogerio Siqueira de Miranda <rogercomp@gmail.com>
2019-08-20 22:27:27 -0300

    Projeto criado

---
+++ src/main/java/com/iftm/prjreferencia/PrjReferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.iftm.prjreferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjReferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjReferenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iftm/prjreferencia/PrjReferenciaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iftm.prjreferencia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrjReferenciaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

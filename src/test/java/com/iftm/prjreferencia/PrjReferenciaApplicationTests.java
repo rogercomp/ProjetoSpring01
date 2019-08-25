579736af9007438ad344b37818ab86f58072ca9b
Rogerio Siqueira de Miranda <rogercomp@gmail.com>
2019-08-25 11:39:58 -0300

    H2 database, test profile, JPA

--- src/test/java/com/iftm/prjreferencia/PrjReferenciaApplicationTests.java
+++ src/test/java/com/iftm/prjreferencia/PrjReferenciaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.iftm.prjreferencia;
+/*package com.iftm.prjreferencia;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class PrjReferenciaApplicationTests {
 	}
 
 }
+*/

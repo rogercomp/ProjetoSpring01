c0083b2460bf0b76a6a9c155a6d60b421510c727
Rogerio Siqueira de Miranda <rogercomp@gmail.com>
2019-08-25 13:06:40 -0300

    Service layer, component registration

---
+++ src/main/java/com/iftm/prjreferencia/services/UserService.java
@@ -0,0 +1,27 @@
+package com.iftm.prjreferencia.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iftm.prjreferencia.entities.User;
+import com.iftm.prjreferencia.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}

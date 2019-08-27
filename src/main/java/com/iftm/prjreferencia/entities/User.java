2120f371e2e447a8393afe88bca4c5fb3c15249e
Rogerio Siqueira de Miranda <rogercomp@gmail.com>
2019-08-27 20:18:58 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/iftm/prjreferencia/entities/User.java
+++ src/main/java/com/iftm/prjreferencia/entities/User.java
@@ -1,13 +1,18 @@
 package com.iftm.prjreferencia.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	
@@ -19,6 +24,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -72,6 +80,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}	
 
 	@Override
 	public int hashCode() {
@@ -96,7 +108,6 @@ public class User implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-	
+	}	
 	
 }

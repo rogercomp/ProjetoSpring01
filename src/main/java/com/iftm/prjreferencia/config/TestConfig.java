7ff845e913c28a6fd41d552d8babd4bf97f7ef40
rogercomp <rogercomp@gmail.com>
2019-09-05 10:22:11 -0300

    Payment, one-to-one association

--- src/main/java/com/iftm/prjreferencia/config/TestConfig.java
+++ src/main/java/com/iftm/prjreferencia/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.iftm.prjreferencia.entities.Category;
 import com.iftm.prjreferencia.entities.Order;
 import com.iftm.prjreferencia.entities.OrderItem;
+import com.iftm.prjreferencia.entities.Payment;
 import com.iftm.prjreferencia.entities.Product;
 import com.iftm.prjreferencia.entities.User;
 import com.iftm.prjreferencia.entities.enums.OrderStatus;
@@ -83,5 +84,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }

67c785819b3da1ce42c89f8717f07d26433e0aed
rogercomp <rogercomp@gmail.com>
2019-10-02 12:42:27 -0300

    Pagination

--- src/main/java/com/iftm/prjreferencia/resources/ProductResource.java
+++ src/main/java/com/iftm/prjreferencia/resources/ProductResource.java
@@ -1,11 +1,13 @@
 package com.iftm.prjreferencia.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,12 +29,17 @@ import com.iftm.prjreferencia.services.ProductService;
 public class ProductResource {
 
 	@Autowired
-	private ProductService service; 
+	private ProductService service;  
 	
 	@GetMapping
-	public ResponseEntity<List<ProductDTO>> findAll() {
-		List<ProductDTO> Products = service.findAll();		
-		return ResponseEntity.ok().body(Products);
+	public ResponseEntity<Page<ProductDTO>> findAllPaged(
+			@RequestParam(value="page", defaultValue = "0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue = "name") String orderby,
+			@RequestParam(value="direction", defaultValue = "ASC") String direction) {
+		PageRequest pagerequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderby);
+		Page<ProductDTO> list = service.findAllPaged(pagerequest);		
+		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")

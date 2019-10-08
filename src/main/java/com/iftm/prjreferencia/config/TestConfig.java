242a67489dd6cebc07db3cba212a78d31f4d8ec0
Rogerio Siqueira de Miranda <rogercomp@gmail.com>
2019-10-08 20:40:27 -0300

    Encoding user password

--- src/main/java/com/iftm/prjreferencia/config/TestConfig.java
+++ src/main/java/com/iftm/prjreferencia/config/TestConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.iftm.prjreferencia.entities.Category;
 import com.iftm.prjreferencia.entities.Order;
@@ -27,6 +28,8 @@ import com.iftm.prjreferencia.repositories.UserRepository;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -72,8 +75,8 @@ public class TestConfig implements CommandLineRunner {
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5)); 
 		
 		
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", passwordEncoder.encode("123456"));
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", passwordEncoder.encode("123456"));		
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		

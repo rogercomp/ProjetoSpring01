77ea2584843c781d60b6f3cc5f043e939a2ae010
rogercomp <rogercomp@gmail.com>
2019-10-16 09:20:13 -0300

    Update Order

--- src/main/java/com/iftm/prjreferencia/resources/OrderResource.java
+++ src/main/java/com/iftm/prjreferencia/resources/OrderResource.java
@@ -9,6 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,44 +24,52 @@ import com.iftm.prjreferencia.services.OrderService;
 public class OrderResource {
 
 	@Autowired
-	private OrderService service; 
-	
+	private OrderService service;
+
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@GetMapping
 	public ResponseEntity<List<OrderDTO>> findAll() {
-		List<OrderDTO> orders = service.findAll();		
+		List<OrderDTO> orders = service.findAll();
 		return ResponseEntity.ok().body(orders);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
 		OrderDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
-	
+
 	@GetMapping(value = "/{id}/items")
 	public ResponseEntity<List<OrderItemDTO>> findItems(@PathVariable Long id) {
 		List<OrderItemDTO> list = service.findItems(id);
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/myorders")
 	public ResponseEntity<List<OrderDTO>> findByClient() {
-		List<OrderDTO> orders = service.findByClient();		
+		List<OrderDTO> orders = service.findByClient();
 		return ResponseEntity.ok().body(orders);
 	}
-	
+
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@GetMapping(value = "/client/{clientId}")
 	public ResponseEntity<List<OrderDTO>> findByClientId(@PathVariable Long clientId) {
-		List<OrderDTO> orders = service.findByClientId(clientId);		 
+		List<OrderDTO> orders = service.findByClientId(clientId);
 		return ResponseEntity.ok().body(orders);
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<OrderDTO> placeOrder(@RequestBody List<OrderItemDTO> dto) {
 		OrderDTO orderDTO = service.placeOrder(dto);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(orderDTO.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(orderDTO.getId())
+				.toUri();
 		return ResponseEntity.created(uri).body(orderDTO);
+	}
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<OrderDTO> update(@PathVariable Long id, @RequestBody OrderDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
 	}
 }
